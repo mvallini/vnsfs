ff899e7a521f7480d233c5f1fdc9f3310d8e424b
Marco Vallini <marco.vallini@polito.it>
2018-04-09 10:39:43 +0200

    second commit

--- juju-charms/juju-env.sh
+++ juju-charms/juju-env.sh
@@ -1,11 +1,4 @@
 # Set the Juju env variables for building a layer
-source deps.sh
-
-# Snap requires confinement on execution into own's home
-if [[ $(get_distro_code_major) -ge 17 ]]; then
-  export JUJU_REPOSITORY=$(echo $HOME)
-else
-  export JUJU_REPOSITORY=$(pwd)
-fi
+export JUJU_REPOSITORY=`pwd`
 export INTERFACE_PATH=$JUJU_REPOSITORY/interfaces
 export LAYER_PATH=$JUJU_REPOSITORY/layers
