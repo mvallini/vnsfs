0b6e0bf13dbb8fac245d74ec31e0fd49de671604
Marco Vallini <marco.vallini@polito.it>
2018-04-10 13:49:09 +0200

    minor fix

--- src/vnf/fl7filter/rest_api/api_calls.py
+++ src/vnf/fl7filter/rest_api/api_calls.py
@@ -8,21 +8,21 @@ from iptables import mspl_translator
 
 def get_rules(handler):
     try:
-        #result = iptables_wrapper.get_iptables_rules()
+		#result = iptables_wrapper.get_iptables_rules()
 		result = None
-        return result
+		return result
     except Exception as e:
         print e
         return False
-    # return subprocess.check_output(["sudo", "iptables", "-L"])
+		# return subprocess.check_output(["sudo", "iptables", "-L"])
 
 
 def set_rules(handler):
     try:
-        payload = handler.get_payload()
+		payload = handler.get_payload()
 		result = None
-        #rules = mspl_translator.xml_to_iptables(payload)
-        #result = iptables_wrapper.set_iptables_rules(rules)
+		#rules = mspl_translator.xml_to_iptables(payload)
+		#result = iptables_wrapper.set_iptables_rules(rules)
 		
 		# write payload to mspl.xml file
 		file = open("/home/centos/utils/fl7filter_mspltranslator/mspl.xml", 'w')
@@ -31,10 +31,10 @@ def set_rules(handler):
 		
 		# execute rule installation
 		result = subprocess.check_output(["sudo", "/home/centos/utils/fl7filter_mspltranslator/mspl_install_conf.sh"])
-        return result
+		return result
     except Exception as e:
-        print e
-        return False
+		print e
+		return False
 
 
 def flush_rule(handler):
@@ -52,7 +52,7 @@ def flush_rules(handler):
     try:
 		result = None
         #result = iptables_wrapper.flush_iptables_rules()
-        return result
+		return result
     except Exception as e:
         print e
         return False
