7bcba1c9d51a3f33698ebd801f89f8b771f50669
Marco Vallini <marco.vallini@polito.it>
2018-04-11 10:31:28 +0200

    fix cloud-init to install lsof add remove previous httpd configuration

--- src/vnf/fl7filter/fl7filter_mspltranslator/reset_conf.sh
+++ src/vnf/fl7filter/fl7filter_mspltranslator/reset_conf.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # restore previous configuration
+sudo rm -f /etc/httpd/conf.d/default-site.conf
 sudo \cp -f /etc/httpd/conf.d.old/conf.d/* /etc/httpd/conf.d/
 sudo \cp -f /etc/httpd/modsecurity.d.old/modsecurity.d/* /etc/httpd/modsecurity.d/ 
 sudo rm -rf /etc/httpd/conf.d.old/
